fe89a663ff1342c732685e6fddb3161e7cb283ad
ljutaev <ljutaev@gmail.com>
2021-07-25 11:43:54 +0300

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify todo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, voluptates, repellat provident odit labore modi consectetur dicta aspernatur odio laudantium saepe omnis illo cumque doloremque nam ipsam, dolor. Sit, ratione!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, voluptates, repellat provident odit labore modi consectetur dicta aspernatur odio laudantium saepe omnis illo cumque doloremque nam ipsam, dolor. Sit, ratione!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, voluptates, repellat provident odit labore modi consectetur dicta aspernatur odio laudantium saepe omnis illo cumque doloremque nam ipsam, dolor. Sit, ratione!</p>
   </div>
 </template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="home">
+    <v-list
+      class="pt-0"
+      flat
+    >
+      <div
+        v-for="task in tasks"
+        :key="task.id"
+      >
+        <v-list-item 
+          @click="doneTask(task.id)"
+        >          
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox :input-value="task.done"></v-checkbox>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{ task.title }}</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>      
+    </v-list>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    data() {
+      return {
+        tasks: [
+          {
+            id: 1,
+            title: 'Wake Up',
+            done: false
+          },
+          {
+            id: 2,
+            title: 'Get Up',
+            done: false
+          }          
+        ]
+      }
+    },
+    methods: {
+      doneTask(id) {
+        console.log(id)
+      }
+    }
+  }
+</script>
