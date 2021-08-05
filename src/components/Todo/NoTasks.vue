8e55c1ae81ff481d6e5e3ce916d150524ca22d56
ljutaev <ljutaev@gmail.com>
2021-08-05 21:57:51 +0300

    child components

---
+++ src/components/Todo/NoTasks.vue
@@ -0,0 +1,26 @@
+<template>
+ <div 
+    class="text-h5 primary--text no-tasks"
+  >
+    <v-icon
+      size="100"
+      class="primary"
+    >
+      mdi-check
+    </v-icon>
+    No tasks
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style lang="sass">
+  .no-tasks
+    position: absolute
+    left: 50%
+    top: 50%
+    transform: translate(-50%, -50%)
+</style>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,54 +1,27 @@
 <template>
   <div class="home">
-    <v-list
-      class="pt-0"
-      flat
-    >
-      <div
-        v-for="task in tasks"
-        :key="task.id"
-      >
-        <v-list-item 
-          @click="doneTask(task.id)"
-        >          
-          <template v-slot:default>
-            <v-list-item-action>
-              <v-checkbox :input-value="task.done"></v-checkbox>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>{{ task.title }}</v-list-item-title>
-            </v-list-item-content>
-          </template>
-        </v-list-item>
-        <v-divider></v-divider>
-      </div>      
-    </v-list>
+    <FieldAddTask />
+
+    <list-tasks
+      v-if="$store.state.tasks.length"
+    />
+
+    <no-tasks v-else />
+
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Home',
-    data() {
-      return {
-        tasks: [
-          {
-            id: 1,
-            title: 'Wake Up',
-            done: false
-          },
-          {
-            id: 2,
-            title: 'Get Up',
-            done: false
-          }          
-        ]
-      }
-    },
-    methods: {
-      doneTask(id) {
-        console.log(id)
-      }
-    }
-  }
+import FieldAddTask from '@/components/Todo/FieldAddTask'
+
+export default {
+  name: 'Home',
+  components: {
+    FieldAddTask,
+    'list-tasks': require('@/components/Todo/ListTasks.vue').default,
+    'no-tasks': require('@/components/Todo/NoTasks.vue').default
+  },
+  
+}
 </script>
+
